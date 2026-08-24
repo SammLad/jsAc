b66c859de31d487097e489977c731c4d32661dad
SammLad <laurasofialadinopelaez@gmail.com>
2026-08-24 09:08:17 -0500

    Implementación de arreglos, .forEach y . map

--- tienda-react/practica-js.js
+++ tienda-react/practica-js.js
@@ -67,3 +67,28 @@ console.log(esMayorEdad(17))
 
 
 
+// ACT 3
+const productos = [
+    { id: 1, nombre: 'Mouse', precio: 50000, stock: 5 },
+    { id: 2, nombre: 'Teclado', precio: 90000, stock: 0 },
+    { id: 3, nombre: 'Monitor', precio: 650000, stock: 3 },
+    { id: 4, nombre: 'Cable USB', precio: 20000, stock: 5 },
+    { id: 5, nombre: 'Cargador celular', precio: 80000, stock: 0 },
+    { id: 6, nombre: 'Estuche portatil', precio: 100000, stock: 3 },
+    { id: 7, nombre: 'Funda celular', precio: 25000, stock: 5 },
+    { id: 8, nombre: 'Audifonos', precio: 150000, stock: 0 },
+    { id: 9, nombre: 'Baterias', precio: 30000, stock: 3 },
+    { id: 10, nombre: 'Vidrio templado', precio: 10000, stock: 5 },
+];
+
+
+
+
+//ACT 4
+productos.forEach(producto => {
+    console.log(producto.nombre);
+});
+
+const nombres = productos.map(producto => {
+    return producto.nombre;
+});
