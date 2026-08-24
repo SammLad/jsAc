36310b52c46f09d9048bba777269c9fb8fdb28c5
SammLad <laurasofialadinopelaez@gmail.com>
2026-08-24 10:49:48 -0500

    feat: ejercicios base de JavaScript

--- tienda-react/practica-js.js
+++ tienda-react/practica-js.js
@@ -58,60 +58,96 @@
 
 
 
-// //ACT 2
+//ACT 2
 // const aplicarIva = precio => precio * 1.19;
 
 // function esMayorEdad(edad) { return edad >= 18; }
 // console.log(aplicarIva(30000))
 // console.log(esMayorEdad(17))
 
+// const cuadrado = numero => numero * numero;
+// const esMayorEdad = edad => {
+//     if (edad > 18) {
+//         return "Es mayor de edad"
+//     } else {
+//         return "Es menor de edad"
+//     }
+// };
+// const nombreCompleto = (nombre, apellido) => nombre + " " + apellido;
+
+// console.log(cuadrado(12));
+// console.log(esMayorEdad(17));
+// console.log(nombreCompleto("Laura", "Ladino"));
 
 
-// // ACT 3
+
+
+
+// ACT 3
 const productos = [
-    { id: 1, nombre: 'Mouse', precio: 50000, stock: 5 },
-    { id: 2, nombre: 'Teclado', precio: 90000, stock: 0 },
-    { id: 3, nombre: 'Monitor', precio: 650000, stock: 3 },
-    { id: 4, nombre: 'Cable USB', precio: 20000, stock: 5 },
-    { id: 5, nombre: 'Cargador celular', precio: 80000, stock: 0 },
-    { id: 6, nombre: 'Estuche portatil', precio: 100000, stock: 3 },
-    { id: 7, nombre: 'Funda celular', precio: 25000, stock: 5 },
-    { id: 8, nombre: 'Audifonos', precio: 150000, stock: 0 },
-    { id: 9, nombre: 'Baterias', precio: 30000, stock: 3 },
-    { id: 10, nombre: 'Vidrio templado', precio: 10000, stock: 5 },
+    { id: 1, nombre: 'Mouse', precio: 50000, categoria: 'Perifericos', stock: 5 },
+    { id: 2, nombre: 'Teclado', precio: 90000, categoria: 'Perifericos', stock: 0 },
+    { id: 3, nombre: 'Monitor', precio: 650000, categoria: 'Pantallas', stock: 3 },
+    { id: 4, nombre: 'Cable USB', precio: 20000, categoria: 'Accesorios', stock: 5 },
+    { id: 5, nombre: 'Cargador celular', precio: 80000, categoria: 'Accesorios', stock: 0 },
+    { id: 6, nombre: 'Estuche portatil', precio: 100000, categoria: 'Accesorios', stock: 3 },
+    { id: 7, nombre: 'Funda celular', precio: 25000, categoria: 'Accesorios', stock: 5 },
+    { id: 8, nombre: 'Audifonos', precio: 150000, categoria: 'Audio', stock: 0 },
+    { id: 9, nombre: 'Baterias', precio: 30000, categoria: 'Accesorios', stock: 3 },
+    { id: 10, nombre: 'Vidrio templado', precio: 10000, categoria: 'Accesorios', stock: 5 },
 ];
 
 
 
 
-// //ACT 4
-// productos.forEach(producto => {
-//     console.log(producto.nombre);
-// });
+//ACT 4
 
-// const nombres = productos.map(producto => {
-//     return producto.nombre;
+// productos.forEach((producto, indice) => {
+//     console.log(`${indice + 1}. ${producto.nombre} - ${producto.precio} - ${producto.stock}`);
 // });
 
 
-// //ACT 5
-// const disponibles = productos.filter(
-//     producto => producto.precio >= 50000 & producto.precio <= 200000
-// );
-
-// const encontrado = productos.find(
-//     producto => producto.id === 5
-// );
-
-// console.log(disponibles);
-// console.log(encontrado);
 
 
+//ACT 5
+// const nombres = productos.map(producto => producto.nombre);
+// const preciosConIva = productos.map(producto => producto.precio * 1.19);
+// const nombresMayuscula = productos.map(producto => producto.nombre.toUpperCase());
+// const resumenProductos = productos.map(producto => producto.nombre + " cuesta " + producto.precio);
+// console.log(nombres);
+// console.log(preciosConIva);
+// console.log(nombresMayuscula);
+// console.log(resumenProductos);
 
 //ACT 6
+
+// const precioMayor = productos.filter(producto => producto.precio > 100000);
+// const precioEntre = productos.filter(
+//     producto => producto.precio >= 50000 & producto.precio <= 200000
+// );
+// const categEscogida = productos.filter(producto => producto.categoria === 'Accesorios');
+// const id5 = productos.find(producto => producto.id === 5);
+// const buscarProducto = (id) => {
+//     return productos.find(producto => producto.id === id);
+// };
+
+// console.log(precioMayor);
+// console.log(precioEntre);
+// console.log(categEscogida);
+// console.log(id5);
+// console.log(buscarProducto(4));
+
+
+
+//ACT 7
+
 const hayAgotados = productos.some(p => p.stock === 0);
 
 const preciosValidos = productos.every(p => p.precio > 0);
+
+const preciosM = productos.every(p => p.precio > 1000000);
+
+const stockM = productos.some(p => p.stock >= 0);
 
 const valorInventario = productos.reduce(
     (total, p) => total + p.precio * p.stock,
@@ -119,10 +155,22 @@ const valorInventario = productos.reduce(
 );
 
 
-console.log(hayAgotados);
-console.log(preciosValidos);
-console.log(valorInventario);
+// console.log("¿Algún producto agotado?:", hayAgotados);
+// console.log("¿Algún precio mayor a $1.000.000?:", preciosM)
+// console.log("¿Todos los precios mayor a $0?:", preciosValidos);
+// console.log("¿Stock igual o mayor a cero?:", stockM);
+// console.log("Valor total inventario:", valorInventario)
 
 
 
-//ACT 7
+//ACT 8
+
+const productoActualizado = {
+    ...productos[5],
+    precio: 90000,
+    stock: 4
+};
+
+const estado = productoActualizado.stock > 0 ? 'Disponible' : 'Agotado';
+
+console.log(`El producto ${productoActualizado.nombre} cuesta $${productoActualizado.precio} y su estado es: ${productoActualizado.stock > 0 ? 'Disponible' : 'Agotado'}`);
