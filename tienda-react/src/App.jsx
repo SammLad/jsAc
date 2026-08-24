276beb1d8f1813ac83fe30df1912ede7fe9c0e96
SammLad <laurasofialadinopelaez@gmail.com>
2026-08-24 11:45:08 -0500

    Cambios por el reto final de autonomía, además de hacer cambios en el css

--- tienda-react/src/App.jsx
+++ tienda-react/src/App.jsx
@@ -3,23 +3,42 @@ import { productos } from './data/productos';
 import './App.css';
 function App() {
   const disponibles = productos.filter(producto => producto.stock > 0);
+  const noDisponibles = productos.some(producto => producto.stock === 0);
   const valorInventario = productos.reduce(
     (total, producto) => total + producto.precio * producto.stock,
     0
   );
+  const estado = noDisponibles ? 'Si hay' : 'No hay';
   return (
     <main className="contenedor">
       <h1>Tienda tecnológica</h1>
       <p>Productos disponibles: {disponibles.length}</p>
       <p>Valor del inventario: ${valorInventario}</p>
-      <section className="productos">
-        {productos.map(producto => (
-          <ProductoCard
-            key={producto.id}
-            producto={producto}
-          />
-        ))}
-      </section>
+      <p>Productos no disponibles: {estado}</p>
+      <div className="secciones">
+        <section className="productos">
+          <h2>Todos los Productos disponibles</h2>
+          {productos.map(producto => (
+            <ProductoCard
+              key={producto.id}
+              producto={producto}
+            />
+          ))}
+        </section>
+        <section className="productos">
+          <h2>Productos disponibles</h2>
+          {
+            disponibles.map(producto => (
+              <ProductoCard
+                key={producto.id}
+                producto={producto}
+              />
+            ))
+          }
+
+
+        </section>
+      </div>
     </main>
   );
 }
